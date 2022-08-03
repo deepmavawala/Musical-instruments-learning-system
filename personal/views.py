5abe9e5fd2b2c8f2499ab608bc41e06562f14306
deepmavawala <deeprm1409@gmail.com>
2022-08-03 22:11:48 +0530

    Initial commit

---
+++ personal/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+from account.models import Account
+
+
+def home_screen_view(request):
+	
+	context = {}
+
+	accounts = Account.objects.all()
+	context['accounts'] = accounts
+
+	return render(request, "personal/home.html", context)
+
+def piano_view(request):
+
+	return render(request, "personal/piano.html")
+
+def test_piano_view(request):
+
+	return render(request, "personal/test_piano.html")
